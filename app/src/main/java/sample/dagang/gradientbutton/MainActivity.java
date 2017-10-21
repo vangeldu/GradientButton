91a5567559e24acb6d4b9e6bc2994a2713c649ce
duwg <duweigang@xmzhen.com>
2017-10-21 21:16:48 +0800

    test jcenter

--- app/src/main/java/sample/dagang/gradientbutton/MainActivity.java
+++ app/src/main/java/sample/dagang/gradientbutton/MainActivity.java
@@ -14,21 +14,20 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        GradientButton magicButton = (GradientButton) findViewById(R.id.magic_button);
-        TextView textView = magicButton.getButton();
+        GradientButton gradientButton = (GradientButton) findViewById(R.id.magic_button);
+        TextView textView = gradientButton.getButton();
         textView.setText("Custom Button");
         textView.setTextColor(Color.BLUE);
-        magicButton.setButtonRadius(20);
-        magicButton.setShadowRadius(10);
-        magicButton.setShadowColor(Color.RED);
-        magicButton.setButtonStartColor(Color.parseColor("#EEE5DE"));
-        magicButton.setButtonEndColor(Color.parseColor("#9370DB"));
-        magicButton.setButtonPressStartColor(Color.BLACK);
-        magicButton.setButtonPressEndColor(Color.BLACK);
-        magicButton.setButtonGradientOrientation(GradientButton.LEFT_RIGHT);
-        magicButton.getButton().setOnClickListener(new View.OnClickListener() {
+        gradientButton.setButtonRadius(20);
+        gradientButton.setShadowRadius(10);
+        gradientButton.setShadowColor(Color.RED);
+        gradientButton.setButtonStartColor(Color.parseColor("#EEE5DE"));
+        gradientButton.setButtonEndColor(Color.parseColor("#9370DB"));
+        gradientButton.setButtonPressStartColor(Color.BLACK);
+        gradientButton.setButtonPressEndColor(Color.BLACK);
+        gradientButton.setButtonGradientOrientation(GradientButton.LEFT_RIGHT);
+        gradientButton.getButton().setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Toast.makeText(MainActivity.this, "onClick", Toast.LENGTH_LONG).show();
